33ce1fb1d808cd05db0f6c2b8aa5b5514adf86e4
Brandon Holt <bholt@cs.washington.edu>
2014-11-09 22:46:39 -0800

    start working on my base environment

---
+++ base/profile.bash
@@ -0,0 +1,10 @@
+
+# set prompt because I'm vain that way
+export PS1='\[\e[0;34m\]docker \[\e[m\]\[\e[0;32m\]\w\[\e[m\] \[\e[0;33m\]> \[\e[m\]'
+
+# common aliases
+alias ls='ls --color=auto'
+alias ll='ls -lah'
+
+# enable tab completion
+source /etc/bash_completion
